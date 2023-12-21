2ee86cefff9cb6ec0688795d403dfdb5429b6439
user1 <user1@DESKTOP-MTS7UCP>
2023-12-21 12:37:12 +0900

    commit

---
+++ AuthorAuction/src/main/java/com/kh/auction/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.kh.auction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/")
+	public String test() {
+		return "hello";
+	}
+}
