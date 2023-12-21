3554e8fd0a2dd6edb77fdf41c2b0578abdb35146
user1 <user1@DESKTOP-MTS7UCP>
2023-12-21 12:21:05 +0900

    First commit

---
+++ AuthorAuction/src/main/java/com/kh/auction/AuthorAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.kh.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorAuctionApplication.class, args);
+	}
+
+}
---
+++ AuthorAuction/src/test/java/com/kh/auction/AuthorAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
