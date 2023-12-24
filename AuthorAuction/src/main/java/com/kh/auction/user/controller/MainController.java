3f526469d87504db154643414ea80ca1356af33f
HSWB <HSWB@DESKTOP-LVEA22O-REP>
2023-12-24 16:42:10 +0900

    Merge branch 'main' of https://github.com/PJiSung/AuthorAuction.git into main

---
+++ AuthorAuction/src/main/java/com/kh/auction/user/controller/MainController.java
@@ -0,0 +1,35 @@
+package com.kh.auction.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kh.auction.member.service.MemberService;
+import com.kh.auction.user.model.vo.Member;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	MemberService mService;
+	
+	@GetMapping("/")
+	public String test(HttpSession session) {
+		Member m = new Member();
+		String id = "123"; //로그인 할 아이디 입력
+		m.setMemId(id);
+		Member loginUser = mService.login(m);
+		session.setAttribute("loginUser", loginUser);
+		return "index";
+	}
+	
+	@GetMapping("/chat")
+	public String chat(@RequestParam(value = "roomNumber", defaultValue = "1") String roomNumber, Model model) {
+		model.addAttribute("roomNumber", roomNumber);
+		return "common/chat";
+	}
+}
