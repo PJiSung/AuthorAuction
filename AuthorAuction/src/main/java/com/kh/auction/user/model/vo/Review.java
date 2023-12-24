16eeda97d40f8d74cc19831a89ad243300789f59
rorong44 <154117557+rorong44@users.noreply.github.com>
2023-12-24 14:17:55 +0900

    리이이뷰

--- AuthorAuction/src/main/java/com/kh/auction/user/model/vo/Review.java
+++ AuthorAuction/src/main/java/com/kh/auction/user/model/vo/Review.java
@@ -6,6 +6,7 @@ public class Review {
 	private int revNo;
 	private String revContent;
 	private int revCount;
+	private int revLike;
 	private Date revCreateDate;
 	private Date revModifyDate;
 	private String revStatus;
@@ -14,12 +15,13 @@ public class Review {
 	private String memFileName;
 	
 	public Review() {}
-	public Review(int revNo, String revContent, int revCount, Date revCreateDate, Date revModifyDate, String revStatus,
-			String revWriter, String memNickName, String memFileName) {
+	public Review(int revNo, String revContent, int revCount, int revLike, Date revCreateDate, Date revModifyDate,
+			String revStatus, String revWriter, String memNickName, String memFileName) {
 		super();
 		this.revNo = revNo;
 		this.revContent = revContent;
 		this.revCount = revCount;
+		this.revLike = revLike;
 		this.revCreateDate = revCreateDate;
 		this.revModifyDate = revModifyDate;
 		this.revStatus = revStatus;
@@ -27,65 +29,93 @@ public class Review {
 		this.memNickName = memNickName;
 		this.memFileName = memFileName;
 	}
+
 	public int getRevNo() {
 		return revNo;
 	}
+
 	public void setRevNo(int revNo) {
 		this.revNo = revNo;
 	}
+
 	public String getRevContent() {
 		return revContent;
 	}
+
 	public void setRevContent(String revContent) {
 		this.revContent = revContent;
 	}
+
 	public int getRevCount() {
 		return revCount;
 	}
+
 	public void setRevCount(int revCount) {
 		this.revCount = revCount;
 	}
+
+	public int getRevLike() {
+		return revLike;
+	}
+
+	public void setRevLike(int revLike) {
+		this.revLike = revLike;
+	}
+
 	public Date getRevCreateDate() {
 		return revCreateDate;
 	}
+
 	public void setRevCreateDate(Date revCreateDate) {
 		this.revCreateDate = revCreateDate;
 	}
+
 	public Date getRevModifyDate() {
 		return revModifyDate;
 	}
+
 	public void setRevModifyDate(Date revModifyDate) {
 		this.revModifyDate = revModifyDate;
 	}
+
 	public String getRevStatus() {
 		return revStatus;
 	}
+
 	public void setRevStatus(String revStatus) {
 		this.revStatus = revStatus;
 	}
+
 	public String getRevWriter() {
 		return revWriter;
 	}
+
 	public void setRevWriter(String revWriter) {
 		this.revWriter = revWriter;
 	}
+
 	public String getMemNickName() {
 		return memNickName;
 	}
+
 	public void setMemNickName(String memNickName) {
 		this.memNickName = memNickName;
 	}
+
 	public String getMemFileName() {
 		return memFileName;
 	}
+
 	public void setMemFileName(String memFileName) {
 		this.memFileName = memFileName;
 	}
+
 	@Override
 	public String toString() {
-		return "Review [revNo=" + revNo + ", revContent=" + revContent + ", revCount=" + revCount + ", revCreateDate="
-				+ revCreateDate + ", revModifyDate=" + revModifyDate + ", revStatus=" + revStatus + ", revWriter="
-				+ revWriter + ", memNickName=" + memNickName + ", memFileName=" + memFileName + "]";
+		return "Review [revNo=" + revNo + ", revContent=" + revContent + ", revCount=" + revCount + ", revLike="
+				+ revLike + ", revCreateDate=" + revCreateDate + ", revModifyDate=" + revModifyDate + ", revStatus="
+				+ revStatus + ", revWriter=" + revWriter + ", memNickName=" + memNickName + ", memFileName="
+				+ memFileName + "]";
 	}
 	
 }
