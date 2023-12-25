9261cf105aa2ea04aa4accfd6735cb83a6a02079
PJiSung <123559958+PJiSung@users.noreply.github.com>
2023-12-25 23:51:40 +0900

    Update WebMvcConfig.java

--- AuthorAuction/src/main/java/com/kh/auction/common/config/WebMvcConfig.java
+++ AuthorAuction/src/main/java/com/kh/auction/common/config/WebMvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 	
-	//사진 파일 경로
+	//사진 파일 경로 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("file:///c:/uploadFiles/", "classpath:/static/");
