5e4c6d5d5e65ed46d7d3ddaf1c19d5a41072ecef
rorong44 <154117557+rorong44@users.noreply.github.com>
2023-12-26 11:37:49 +0900

    Create ReviewException.java

---
+++ AuthorAuction/src/main/java/com/kh/auction/user/ReviewException.java
@@ -0,0 +1,7 @@
+package com.kh.auction.user.exception;
+
+public class ReviewException extends RuntimeException {
+	
+	public ReviewException() {} 
+	public ReviewException(String msg) {super(msg);}
+}
